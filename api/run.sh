5efe603459e29238d92ce2e8483eccf41d6b2a23
michaeld-sh <mdoyleazz@gmail.com>
2019-03-19 17:43:25 -0700

    first commit

---
+++ api/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+# Wait for Postgres to become available.
+until psql -h db -U "postgres" -c '\q' 2>/dev/null; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+cd /app/api
+mix ecto.create
+mix ecto.migrate
+mix phx.server
