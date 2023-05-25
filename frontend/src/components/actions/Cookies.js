d66a9b44b6ffc65383b9d2d979de1f556cf78324
Michael Doyle <michael.doyle@oforce.com>
2023-05-25 10:48:01 -0700

    Thing

--- frontend/src/components/actions/Cookies.js
+++ frontend/src/components/actions/Cookies.js
@@ -14,7 +14,7 @@ const writeCookie = (cookieName, value) => {
 
     bake_cookie(cookieName, cookieData, expireDate); // Adds quote id to cookies
   } else {
-    console.log("Creating Cookie");
+    console.log("Creating Cookies");
     bake_cookie(cookieName, [value]); // Adds new array to cookies
   };
 };
